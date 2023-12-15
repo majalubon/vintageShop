d41256b5af4b2d0f80424d76107b9b3d8abfa176
majalubon <mlubo001@doc.gold.ac.uk>
2023-12-15 00:29:17 +0000

    update

--- routes/main.js
+++ routes/main.js
@@ -39,7 +39,7 @@ module.exports = function(app, shopData) {
         if (req.session.userId) {
             next();
         } else {
-            res.redirect('/login');
+            res.redirect('login.ejs');
         }
     }
     app.use(session({
